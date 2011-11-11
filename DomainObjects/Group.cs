1035e91f27f9bdb80f117d383a975ad6270a4f85
Venelin Valkov <venelin@naughtyspirit.com>
2011-11-11 20:12:22 +0200

    Added Login, Admin, User List views

--- DomainObjects/Group.cs
+++ DomainObjects/Group.cs
@@ -9,5 +9,6 @@ namespace NaughtySpirit.StoreManager.DomainObjects
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public IList<User> Users { get; set; }
     }
 }
--- WpfApplication/App.xaml.cs
+++ WpfApplication/App.xaml.cs
@@ -12,5 +12,11 @@ namespace WpfApplication
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            this.StartupUri = new System.Uri("/LoginView.xaml", System.UriKind.Relative);
+
+        }
     }
 }
