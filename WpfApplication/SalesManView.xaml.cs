4b41effa6dee83b558cc3b10fd2d000649458715
Venelin Valkov <venelin@naughtyspirit.com>
2011-11-16 11:31:49 +0200

    Added leaner master -> detail binding, Added filtering to the supplier and products

---
+++ WpfApplication/SalesManView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for SalesManView.xaml
+    /// </summary>
+    public partial class SalesManView : Window
+    {
+        public SalesManView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfApplication/SalesManagement/AddProductView.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using NaughtySpirit.StoreManager.DomainObjects;
+using NaughtySpirit.StoreManager.DataLayer;
+
+namespace WpfApplication.SalesManagement
+{
+    /// <summary>
+    /// Interaction logic for AddProductView.xaml
+    /// </summary>
+    public partial class AddProductView : Window
+    {
+        private readonly StorageContext dataContext = new StorageContext();
+
+        public AddProductView()
+        {
+            InitializeComponent();
+            var suppliersQuery = from supplier in dataContext.Suppliers.Include("Products") select supplier;
+            var suppliers = (Suppliers) Resources["suppliers"];
+            foreach (var supplier in suppliersQuery) 
+            {
+                suppliers.Add(supplier);
+            }
+        }
+
+        private void CollectionViewSource_Filter(object sender, FilterEventArgs e)
+        {
+            Supplier s = e.Item as Supplier;
+            if (s != null)
+            // If filter is turned on, filter completed items.
+            {
+                if (s.Name.Contains(FilterBox.Text))
+                {
+                    e.Accepted = true;
+                }
+                else
+                {
+                    e.Accepted = false;
+                }
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(SupplierList.ItemsSource).Refresh();
+        }
+
+        private void CollectionViewSource_Filter_1(object sender, FilterEventArgs e)
+        {
+            
+            Product s = e.Item as Product;
+            if (s != null)
+            // If filter is turned on, filter completed items.
+            {
+                if (s.Name.Contains(ProductFilterBox.Text))
+                {
+                    e.Accepted = true;
+                }
+                else
+                {
+                    e.Accepted = false;
+                }
+            }
+        }
+
+        private void ProductFilterBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(ProductList.ItemsSource).Refresh();
+        }
+    }
+
+    public class Suppliers : ObservableCollection<Supplier>
+    { }
+
+    public class Products : ObservableCollection<Product>
+    { }
+}
